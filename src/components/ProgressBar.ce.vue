58218b5a858d1e992b7fdd878828e36cdd5e5d5f
Nekonyx13 <corvin.prath@gmail.com>
2023-04-18 21:12:48 +0200

    finished documentation + small fixes

--- src/components/ProgressBar.ce.vue
+++ src/components/ProgressBar.ce.vue
@@ -11,11 +11,11 @@ defineProps<{
       class="h-full rounded-full progress transition-[width] duration-500 sd-progress"
       :style="{ width: `${progress}%` }"
       :class="[
-        { 'sd-progress-20': progress > 20 },
-        { 'sd-progress-40': progress > 40 },
-        { 'sd-progress-60': progress > 60 },
-        { 'sd-progress-80': progress > 80 },
-        { 'sd-progress-100': progress > 100 },
+        { 'sd-progress-20': progress >= 20 },
+        { 'sd-progress-40': progress >= 40 },
+        { 'sd-progress-60': progress >= 60 },
+        { 'sd-progress-80': progress >= 80 },
+        { 'sd-progress-100': progress >= 100 },
       ]"
     ></div>
   </div>
